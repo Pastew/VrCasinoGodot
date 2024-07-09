f03cc32cda27446674c4c0bb314d63feb93f85aa
Pastew <kpastew@gmail.com>
2024-07-09 22:38:04 +0200

    feat: debug button on machine

--- VrCasino/SlotMachineController.gd
+++ VrCasino/SlotMachineController.gd
@@ -8,8 +8,7 @@ extends Node3D
 @export var debug_button: bool = false : set = debug
 		
 func debug(new_value: bool) -> void:
-	pass
-	#_on_play_button_pressed()
+	_notify_game_manager_spin_button_pressed()
 	
 @export var url: String
 
@@ -24,6 +23,9 @@ func _process(delta):
 	pass
 
 func _on_play_button_pressed(_button) -> void:
+	_notify_game_manager_spin_button_pressed()
+	
+func _notify_game_manager_spin_button_pressed():
 	var anchor_node = get_node(WEB_BROWSER_ANCHOR)
 	var game_manager = get_node("%GameManager")
 	
